064583a0d024bdfc5e721b6ba4df64ac51630c63
Serega007 <ermaksochi@gmail.com>
2024-12-08 21:53:43 +0300

    Отказ от некоторых функций фонового режима

--- visible.js
+++ visible.js
@@ -11,35 +11,4 @@
             }
         }
     })
-
-    // window.addEventListener = new Proxy(window.addEventListener, {
-    //     apply(target, self, args) {
-    //         if (args && args?.[1] && args?.[0] === 'animationend') {
-    //             setTimeout(() => {
-    //                 args[1]()
-    //             }, 1)
-    //         } else {
-    //             return Reflect.apply(target, self, args)
-    //         }
-    //     }
-    // })
-
-    /* requestAnimationFrame */
-    let lastTime = 0
-    window.requestAnimationFrame = new Proxy(window.requestAnimationFrame, {
-        apply(target, self, args) {
-            const currTime = Date.now()
-            const timeToCall = Math.max(0, 16 - (currTime - lastTime))
-            const id = setTimeout(function() {
-                args[0](performance.now())
-            }, timeToCall)
-            lastTime = currTime + timeToCall
-            return id
-        }
-    })
-    window.cancelAnimationFrame = new Proxy(window.cancelAnimationFrame, {
-        apply(target, self, args) {
-            clearTimeout(args[0])
-        }
-    })
 }
